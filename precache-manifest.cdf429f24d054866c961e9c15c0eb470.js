f2a5f69731ca0322f752d21787ce2e3d6b02438c
Darío LM <hello@dariolm.com>
2020-09-13 18:11:31 +0200

    Updates

---
+++ precache-manifest.cdf429f24d054866c961e9c15c0eb470.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce67550021007a38f0b3b3d10454635",
+    "url": "/react-empathy-app/index.html"
+  },
+  {
+    "revision": "3b6802ea309ad9f228f6",
+    "url": "/react-empathy-app/static/css/main.7c96d031.chunk.css"
+  },
+  {
+    "revision": "5bf742e1e6950f816ed3",
+    "url": "/react-empathy-app/static/js/2.12337208.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-empathy-app/static/js/2.12337208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76db50d032781009870f",
+    "url": "/react-empathy-app/static/js/3.35dd76df.chunk.js"
+  },
+  {
+    "revision": "3b6802ea309ad9f228f6",
+    "url": "/react-empathy-app/static/js/main.9fb82ce4.chunk.js"
+  },
+  {
+    "revision": "9a32f5359ad4d8599500",
+    "url": "/react-empathy-app/static/js/runtime-main.1cfec30a.js"
+  },
+  {
+    "revision": "ae4ceedccd8a221868f05f976b336f97",
+    "url": "/react-empathy-app/static/media/emphaty-logo.ae4ceedc.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-empathy-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
