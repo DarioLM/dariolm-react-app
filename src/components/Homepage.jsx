8acad4ec083aaa8357c0dfc5247909b1a2ffcb69
Darío LM <hello@dariolm.com>
2020-09-13 03:58:46 +0200

    Readme Update + First version of Main Page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,30 +1,35 @@
 import React, { useState } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
-import loadable from '@loadable/component'
-import Button from "@dariolm/react-common-components-library/build/button";
+import loadable from '@loadable/component';
+import TabsPanel from "@dariolm/react-common-components-library/modules/tabsPanel";
 
-const LinkComponent = loadable(() => import("@dariolm/react-common-components-library/build/link"), {
+const LinkComponent = loadable(() => import("@dariolm/react-common-components-library/modules/link"), {
   fallback: <div>Cargando...</div>
 });
 
-const Homepage = () => {
-  const [tabSelected, setTabSelected] = useState(false);
-  return (
+const tabsConfig = [
+  {
+    label: "Empty",
+    value: 0,
+    content: null,
+  },
+  {
+    label: "Link",
+    value: 1,
+    content: <LinkComponent to={"https://www.empathy.co/"}/>,
+  }
+];
+
+const Homepage = () => (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div onClick={() => setTabSelected(!tabSelected)}>
-         TabPanel
-         {!tabSelected ?
-          <Button />
-          :
-          <LinkComponent to={"https://www.empathy.co/"}/>
-        }
+        <div>
+         <TabsPanel tabs={tabsConfig} />
         </div>
       </header>
     </div>
   );
-}
 
 export default Homepage;
